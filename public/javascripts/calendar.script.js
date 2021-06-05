6af11b8e19cc6ff2ea34a6d4f23d31663063274e
Laura Kapitza <laura.kapitza@gmail.com>
2021-06-05 11:17:45 +0200

    week nav btn DONE

--- public/javascripts/calendar.script.js
+++ public/javascripts/calendar.script.js
@@ -1,5 +1,5 @@
 const $btnPreviousWeek = document.querySelector('.btn-previous-week');
-const $btnNextWeek =document.querySelector('.btn-previous-week');
+const $btnNextWeek =document.querySelector('.btn-next-week');
 const $p = document.querySelector('.current-week');
 
 
@@ -7,14 +7,11 @@ const $p = document.querySelector('.current-week');
 ////////////////////////// FUNCTIONS ///////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
-function startOfWeek(date) {
-  let firstDay = date.getDate() - date.getDay() + (date.getDay === 0 ? -6 : 1)
-  return new Date(date.setDate(firstDay))
-}
+// CURRENT WEEK
 
-function startOfNextWeek(date){
-  let nextweek = new Date(date.getFullYear(), date.getMonth(), date.getDate()+7);
-  return nextweek;
+function startOfWeek(date) {
+  let firstDay = date.getDate() - date.getDay() + (date.getDay === 0 ? -6 : 1);
+  return new Date(date.setDate(firstDay))
 }
 
 function endOfWeek(date) {
@@ -26,29 +23,53 @@ function getFormattedDate(date) {
   let dd = String(date.getDate());
   let mm = String(date.getMonth() + 1);
   let yy = String(date.getFullYear());
-
+  
   if (mm.length < 2) mm = '0' + mm;
   if (dd.length < 2) dd = '0' + dd;
-
+  
   return `${dd}/${mm}/${yy}`
 }
-
 
 function showCurrentWeek() {
   let today = new Date()
   let firstDay = startOfWeek(today)
   let lastDay = endOfWeek(today)
-
+  
   let firstDayFormatted = getFormattedDate(firstDay);
   let lastDayFormatted = getFormattedDate(lastDay)
-
-  console.log('first day', firstDayFormatted)
-  console.log('last day', lastDayFormatted)
-
+  
   $p.innerHTML = `${firstDayFormatted} - ${lastDayFormatted}`
 }
 
 showCurrentWeek()
+
+// NEXT WEEK
+
+function startOfNextWeek(date){
+  let day = new Date(date.getTime() + 24 * 60 * 60 * 1000)
+  let week = new Date(day.getFullYear(), day.getMonth(), day.getDate()+6);
+  return week;
+}
+
+// PREVIOUS WEEK
+
+function startOfPreviousWeek(date){
+  let day = new Date(date.getTime() - 24 * 60 * 60 * 1000)
+  let week = new Date(day.getFullYear(), day.getMonth(), day.getDate()-6);
+  return week;
+}
+
+// UPDATE WEEK
+
+function updateWeek(date) {
+  let firstDay = startOfWeek(date)
+  let lastDay = endOfWeek(date)
+  
+  let firstDayFormatted = getFormattedDate(firstDay);
+  let lastDayFormatted = getFormattedDate(lastDay)
+  
+  $p.innerHTML = `${firstDayFormatted} - ${lastDayFormatted}`
+}
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -56,10 +77,15 @@ showCurrentWeek()
 ////////////////////////////////////////////////////////////////////////
 
 $btnNextWeek.addEventListener('click', (e) => {
-  // let firstDay = $p.innerHTML
-  let nextWeek = startOfWeek(firstDay)
-}, false);
+  let arr = $p.innerHTML.slice(0, 10).split("/");
+  let date = new Date(`${arr[2]}/${arr[1]}/${arr[0]}`);
+  let nextWeek = startOfNextWeek(date);
+  updateWeek(nextWeek);
+});
 
 $btnPreviousWeek.addEventListener('click', (e) => {
-  console.log('previous weekti')
-}, false);
+  let arr = $p.innerHTML.slice(0, 10).split("/");
+  let date = new Date(`${arr[2]}/${arr[1]}/${arr[0]}`);
+  let previousWeek = startOfPreviousWeek(date);
+  updateWeek(previousWeek);
+});
